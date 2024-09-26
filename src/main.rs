c101b3ede3e5b6d82c74707fa937525920c4e160
TegranGrigorian <tegrgrigor@gmail.com>
2024-09-25 22:02:08 -0400

    gigapackiong

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,31 @@
 //include command line i/o
-use std::{char, io::{self, Read}};
+#[allow(non_snake_case)]
+
+use std::{char, collections::HashMap, env::consts, io::{self, Read}};
+use ordered_float::OrderedFloat;
+
 fn assigmentOptions() {
-    println!("")
-    //printf("What assigment do you want to add?\n\n");
-// printf("Enter what assigment you want to input in. Q = quiz, L = Lab, W = Worksheet, R = Reading, E = Exam, P = Lab Practicums\n");
+    let mut assigmentInput: String = String::new();
+    println!("Enter what assigment you want to input in. Q = quiz, L = Lab, W = Worksheet, R = Reading, E = Exam, P = Lab Practicums\n");
+    io::stdin().read_line(&mut assigmentInput).expect("please enter a valid character!");
+    let mut assigmentInput: char = assigmentInput.chars().next().unwrap();
+    match assigmentInput {
+        'Q' => {quizInfo();}
+        'L'=>println!("urmom"),
+        _=>println!("dude")
+    };
+}
+fn quizInfo() {
+    let mut quizAssigmentsGrades: HashMap<String, OrderedFloat<f64>> = HashMap::new();
+    
+    let QC: f32 = 0.2; //constant assoicated with quizzes
+    println!("The Current Quiz Grades:");
+    quizAssigmentsGrades.insert(String::from("Quiz 1"), OrderedFloat(0.97));
+    for (value, key,) in &quizAssigmentsGrades {
+        // key is of type OrderedFloat<f64>, so we dereference it to get the inner f64 value
+        println!("\t{}: Grade = {:.2}%", value, key.0 * 100.0);
+    }
+
 }
 fn main() {
     let currentGrade: f32 = 0.0;
@@ -18,7 +40,8 @@ fn main() {
         println!("Do you want to add an assigment, enter  'E'. If you want to view gradebook enter 'V'. If you want to leave enter 'L'");
         io::stdin().read_line(&mut userInput).expect("enter in value");
         if (userInput.chars().next().unwrap() == 'E') {
-            
+            println!("What assigment do you want to add?\n\n");
+            assigmentOptions();
         }
         // } else if ()  {
     
